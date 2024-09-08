c8ec2c1b6e7688b04540f057ed2d832f01c94572
AVK <avkbond007@gmail.com>
2024-09-08 14:47:46 +0530

    basic hello world c program

---
+++ hello_ji.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+void main()
+{
+    
+    printf("Wlcome to Code With vermaji \n");
+
+    for(int x=0;x<10;x++)
+    {
+        printf("[%d] Hello World \n",x+1);
+    }
+    
+    printf("Thankyou :) \n");
+}
