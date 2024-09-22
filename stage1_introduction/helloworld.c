e2656e978917461228d77710a2baebeaa823a611
AVK <avkbond007@gmail.com>
2024-09-22 15:54:56 +0530

    adding the Env_set_Up giude and 1st c program

---
+++ stage1_introduction/helloworld.c
@@ -0,0 +1,24 @@
+/**
+ * @file    helloworld.c
+ * @brief   print Hello World
+ *
+ * @Folder   git\C\stage1_introduction\helloworld.c
+ * @author   Akash Verma
+ * @date     
+ *
+ * @verbatim
+ *  Date        Author        CR #       Description
+ *  ------      --------      ----       --------------
+ *  22/09/24    Akash                    Print the Hello World
+ *  
+ * @endverbatim
+ **/
+
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World :) \n");
+    return 0;
+}
